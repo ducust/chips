218a8255f5b0c38416224823eb70d44a2add158a
HonCustin <ducust@outlook.com>
2017-08-20 23:07:34 +0800

    mergeTrees
    // this solution is copy by shawngao

--- src/main/java/com/ducust/algorithm/leetcode/tree/MergeTrees.java
+++ src/main/java/com/ducust/algorithm/leetcode/tree/MergeTrees.java
@@ -6,10 +6,14 @@ package com.ducust.algorithm.leetcode.tree;
 public class MergeTrees {
 
 
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) { // this solution is copy by shawngao
 
-
-        return new TreeNode(0);
+        if (t1 == null && t2 == null) return null;
+        int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+        TreeNode newTree = new TreeNode(val);
+        newTree.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        newTree.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+        return newTree;
     }
 
 
@@ -32,10 +36,6 @@ public class MergeTrees {
 
 
     public static void main(String[] args) {
-
-        TreeNode t1 = new TreeNode(0);
-
-        TreeNode t2 = new TreeNode(0);
 
     }
 
